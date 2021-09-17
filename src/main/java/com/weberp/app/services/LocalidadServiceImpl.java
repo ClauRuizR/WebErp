55be464a7d0a2623b14589afbb01199017615ec0
Claudio Ruiz <claudio.ruiz07@gmail.com>
2021-09-17 16:24:09 -0400

    changing build

--- src/main/java/com/weberp/app/services/LocalidadServiceImpl.java
+++ src/main/java/com/weberp/app/services/LocalidadServiceImpl.java
@@ -7,7 +7,7 @@ import com.weberp.app.dto.LocalidadDTO;
 import com.weberp.app.dto.config.ConfigMapper;
 import com.weberp.app.repositories.LocalidadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;    
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
